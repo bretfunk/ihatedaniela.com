12ebd2ccf657f9ed80b674c7961d52979668e017
Bret Funk <bret.e.funk@gmail.com>
2024-04-01 10:35:52 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,50 +71,15 @@ const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=de
 
 const IndexPage = () => (
   <Layout>
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
-    </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
+  <div style={{textAlign: "center", marginTop: "100px"}}>
+  <h1 style={{color: "red", fontSize: "150px"}}>April Fools!</h1>
+  <div style={{marginTop: "100px"}}>
+      <a href="http://www.ilovedaniela.com">
+        <br/>
+    <button style={{background: "blue", color: "white", fontSize: "24px", padding: "10px"}}>ilovedaniela.com</button>
+  </a>
+  </div>
+  </div>
   </Layout>
 )
 
